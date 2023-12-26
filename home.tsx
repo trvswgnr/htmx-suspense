21dda8295fd6e5f1b86fa75cd36738c38b04e22d
Travis Wagner <trvswgnr@gmail.com>
2023-12-26 04:32:13 -0500

    Add stylesheet and heading for posts

--- home.tsx
+++ home.tsx
@@ -5,9 +5,11 @@ export default function Home() {
             <head>
                 <title>htmx-suspense</title>
                 <script src="https://unpkg.com/htmx.org@1.9.10"></script>
+                <link rel="stylesheet" href="/public/style.css" />
             </head>
             <body>
                 <h1>htmx-suspense</h1>
+                <h2>Posts</h2>
                 <Posts />
             </body>
         </html>
