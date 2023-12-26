8a4069199cfdc4e06670653cc446bc0af5dfa0c6
Travis Wagner <trvswgnr@gmail.com>
2023-12-26 04:32:02 -0500

    Refactor Posts component to use server-side rendering with SQLite database

--- Posts.tsx
+++ Posts.tsx
@@ -1,54 +1,43 @@
+/// <reference lib="dom" />
+
 import HtmxSuspense from "./htmx-suspense";
+import { z } from "zod";
 
 export default function Posts() {
-    return <HtmxSuspense fn={async () => {
-        const userRes = await fetch("https://jsonplaceholder.typicode.com/users/1");
-        const user = await userRes.json() as User;
-        const postsRes = await fetch("https://jsonplaceholder.typicode.com/posts?userId=1");
-        const posts = await postsRes.json() as Post[];
-        return (
-            <div className="posts">
-                <h2>{user.name}</h2>
-                <ul>
-                    {posts.map((post) => (
-                        <li key={post.id}>
-                            <h3>{post.title}</h3>
-                            <p>{post.body}</p>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        );
-    }} />
+    return (
+        <HtmxSuspense
+            server={serverAction}
+            client={clientAction}
+        >
+            <div className="htmx-suspense-fallback">Loading...</div>
+        </HtmxSuspense>
+    );
 }
 
-type Post = {
-    userId: number;
-    id: number;
-    title: string;
-    body: string;
-}
-
-type User = {
-    id: number;
-    name: string;
-    username: string;
-    email: string;
-    address: {
-        street: string;
-        suite: string;
-        city: string;
-        zipcode: string;
-        geo: {
-            lat: string;
-            lng: string;
-        };
-    };
-    phone: string;
-    website: string;
-    company: {
-        name: string;
-        catchPhrase: string;
-        bs: string;
-    };
+const serverAction = async () => {
+    const { Database } = await import("bun:sqlite");
+    const { z } = await import("zod");
+    const db = Database.open("db.sqlite");
+    const Post = z.object({
+        userId: z.number(),
+        id: z.number(),
+        title: z.string(),
+        body: z.string(),
+    });
+    type Post = z.infer<typeof Post>;
+    const rows = db.prepare("SELECT * FROM posts").all().map((row) => Post.parse(row));
+    return <div className="posts">
+        {rows.map((post) => <div className="post" key={post.id}>
+            <h2 className="post-title">{post.title}</h2>
+            <p className="post-body">{post.body}</p>
+        </div>)}
+    </div>;
 };
+
+const clientAction = (document: Document) => {
+    console.log("hello from client");
+    const posts = document.querySelector(".posts");
+    if (posts) {
+        posts.classList.add("loaded");
+    }
+}
